51535da1d885a26c11874639293885b1518426df
Junaeed Bin Saif <junaeed.cse@gmail.com>
2025-10-13 23:55:30 +0600

    Add files via upload

---
+++ Phitron/Introduction to programming/Practice day 01/Optional(Pattern)/Pattern_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int j = n; j >= 1; j--)
+    {
+        for (int i = n; i >= 1; i--)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+        n--;
+    }
+    return 0;
+}
