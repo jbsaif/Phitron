d6e712219495b9dfd950e6707db9726f79aeb789
Junaeed Bin Saif <junaeed.cse@gmail.com>
2025-12-20 21:33:33 +0600

    Add files via upload

---
+++ BASIC DATA STRUCTURES/MODULE 01/Logarithmic_complexity_O_logn.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    // for(int i=1; i<=n; i*=2) // 10 ta operation   log2(n)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    for(int i=n; i>=1; i/=3)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
